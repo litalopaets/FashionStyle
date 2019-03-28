65023686710d230b9c03316b3a8b73294fa33dad
lital <litalopaets@gmail.com>
2019-03-28 16:07:04 +0200

    connection to firebase

--- app/src/main/java/com/example/admin/fashionstyle/StartActivity.java
+++ app/src/main/java/com/example/admin/fashionstyle/StartActivity.java
@@ -3,12 +3,12 @@ package com.example.admin.fashionstyle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //start
+
     }
 }
