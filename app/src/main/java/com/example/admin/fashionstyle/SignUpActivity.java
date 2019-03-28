5cb73c5fe46136d93a2555c4c94dc2923e145e57
lital <litalopaets@gmail.com>
2019-03-28 17:26:04 +0200

    login activity+ sign up code

--- app/src/main/java/com/example/admin/fashionstyle/SignUpActivity.java
+++ app/src/main/java/com/example/admin/fashionstyle/SignUpActivity.java
@@ -1,13 +1,119 @@
 package com.example.admin.fashionstyle;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText username, fullname, email, password;
+    Button SignUp;
+    TextView txt_login;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        username = findViewById(R.id.username);
+        fullname = findViewById(R.id.fullname);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        SignUp = findViewById(R.id.SignUp);
+        txt_login = findViewById(R.id.txt_login);
+
+
+        auth = FirebaseAuth.getInstance();
+        txt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            }
+        });
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pd = new ProgressDialog(SignUpActivity.this);
+                pd.setMessage("Please Wait...");
+                pd.show();
+
+
+                String str_username = username.getText().toString();
+                String str_fullname = fullname.getText().toString();
+                String str_email = email.getText().toString();
+                String str_password = password.getText().toString();
+
+                if (TextUtils.isEmpty(str_username) || TextUtils.isEmpty(str_fullname) || TextUtils.isEmpty(str_email) || TextUtils.isEmpty(str_password)) {
+                    Toast.makeText(SignUpActivity.this, "All fields are required!", Toast.LENGTH_SHORT).show();
+                } else if (str_password.length() < 6) {
+                    Toast.makeText(SignUpActivity.this, "Password must have 6 characters", Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
+
+            }
+        });
     }
+
+    private void SignUp(final String username, final String fullname, String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+
+                            reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
+                            HashMap<String, Object> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username.toLowerCase());
+                            hashMap.put("fullname", fullname);
+                            hashMap.put("bio", "");
+                            hashMap.put("imageurl", "https://firebasestorage.googleapis.com/v0/b/fashionstyle2.appspot.com/o/placeholderImg.jpeg?alt=media&token=2560aa89-82f6-4f42-987f-3d0465fc82a3");
+
+
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        pd.dismiss();
+                                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                        } else {
+                            pd.dismiss();
+                            Toast.makeText(SignUpActivity.this, "You can't Sign up with this email or password", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
 }
