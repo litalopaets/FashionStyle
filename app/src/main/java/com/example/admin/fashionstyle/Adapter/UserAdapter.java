a60284b561f280a8e4bc5427761e933a4020162f
daniel <leibman.daniel@gmail.com>
2019-03-31 19:53:35 +0300

    Search users and follow design

---
+++ app/src/main/java/com/example/admin/fashionstyle/Adapter/UserAdapter.java
@@ -0,0 +1,54 @@
+package com.example.admin.fashionstyle.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.admin.fashionstyle.Model.User;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+
+
+    private Context mContext;
+    private List<User> mUsers;
+
+    private FirebaseUser firebaseUser;
+
+
+    public UserAdapter(Context mContext, List<User> mUsers) {
+        this.mContext = mContext;
+        this.mUsers = mUsers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(mContext).inflate(android.support.v4.R.layout.user_item,viewGroup,false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
+}
